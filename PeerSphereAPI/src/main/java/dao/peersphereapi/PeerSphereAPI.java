47d467f8bc560a919443faec13f67b4cdb4bab20
Javier Vargas <3231javiervargas@gmail.com>
2025-10-30 11:48:25 -0400

    humbler beginnings

---
+++ PeerSphereAPI/src/main/java/dao/peersphereapi/PeerSphereAPI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dao.peersphereapi;
+
+/**
+ *
+ * @author thebruh
+ */
+public class PeerSphereAPI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
